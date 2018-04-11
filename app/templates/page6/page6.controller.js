3604ef06f7c76b8de1f95223e5383b7baea20afb
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-04-12 01:41:29 +0300

    page 6

--- app/templates/page6/page6.controller.js
+++ app/templates/page6/page6.controller.js
@@ -5,15 +5,25 @@
         .controller('Page6Controller', Page6Controller);
 
 
-    Page6Controller.$inject = ['$state', 'toastr', '$scope'];
+    Page6Controller.$inject = ['$state', 'toastr', '$scope', '$timeout'];
 
-    function Page6Controller($state, toastr, $scope) {
+    function Page6Controller($state, toastr, $scope, $timeout) {
         let vm = this;
 
         vm.next = next;
 
         function next() {
-                $state.go('page7')
+            if(vm.page6 == undefined ||vm.page6.C === undefined || vm.page6.i === undefined || vm.page6.j === undefined ){
+                toastr.warning('Заповніть усі поля', 'Увага');
+            } else if(vm.page6.C === 'c' && vm.page6.i === 'i' && vm.page6.j === 'j'){
+                toastr.success( 'Вірно');
+                $timeout( function(){
+                    $state.go('page7')
+                }, 3000 );
+            } else {
+                toastr.warning('Вірна відровідь Cij', 'Увага');
+            }
+
         }
     }
 })();
