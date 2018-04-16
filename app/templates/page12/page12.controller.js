8d941ad3d583e4518e832279dac82d4ab6321680
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-04-16 22:05:22 +0300

    add pages 12 ,13

--- app/templates/page12/page12.controller.js
+++ app/templates/page12/page12.controller.js
@@ -5,18 +5,25 @@
         .controller('Page12Controller', Page12Controller);
 
 
-    Page12Controller.$inject = ['$state', 'toastr', '$scope'];
+    Page12Controller.$inject = ['$state', 'toastr', '$scope', '$timeout'];
 
-    function Page12Controller($state, toastr, $scope) {
+    function Page12Controller($state, toastr, $scope, $timeout) {
         let vm = this;
 
         vm.next = next;
 
         function next() {
+            if (vm.page12 == undefined || vm.page12.n === undefined || vm.page12.i0 === undefined || vm.page12.X === undefined || vm.page12.i1 === undefined || vm.page12.j1 === undefined || vm.page12.a === undefined || vm.page12.i2 === undefined) {
+                toastr.warning('Заповніть усі поля', 'Увага');
 
-                $state.go('page13')
-
-
+            } else if ( vm.page12.n === 'n' && vm.page12.i0 === '1' && vm.page12.X === 'x' && vm.page12.i1 === 'i' && vm.page12.j1 === 'j' && vm.page12.a === 'a' && vm.page12.i2 === 'i') {
+                toastr.success('Вірно');
+                $timeout(function () {
+                    $state.go('page13')
+                }, 2000);
+            } else {
+                toastr.warning('Вірна відровідь інакша', 'Увага');
+            }
         }
     }
 })();
--- app/templates/page13/page13.controller.js
+++ app/templates/page13/page13.controller.js
@@ -5,17 +5,27 @@
         .controller('Page13Controller', Page13Controller);
 
 
-    Page13Controller.$inject = ['$state', 'toastr', '$scope'];
+    Page13Controller.$inject = ['$state', 'toastr', '$scope','$timeout'];
 
-    function Page13Controller($state, toastr, $scope) {
+    function Page13Controller($state, toastr, $scope, $timeout) {
         let vm = this;
 
         vm.next = next;
 
         function next() {
 
-                $state.go('page14')
 
+            if (vm.page13 == undefined || vm.page13.m === undefined || vm.page13.i0 === undefined || vm.page13.X === undefined || vm.page13.i1 === undefined || vm.page13.j1 === undefined || vm.page13.b === undefined || vm.page13.j2 === undefined) {
+                toastr.warning('Заповніть усі поля', 'Увага');
+
+            } else if ( vm.page13.m === 'm' && vm.page13.i0 === '1' && vm.page13.X === 'x' && vm.page13.i1 === 'i' && vm.page13.j1 === 'j' && vm.page13.b === 'b' && vm.page13.j2 === 'j') {
+                toastr.success('Вірно');
+                $timeout(function () {
+                    $state.go('page14')
+                }, 2000);
+            } else {
+                toastr.warning('Вірна відровідь інакша', 'Увага');
+            }
 
         }
     }
