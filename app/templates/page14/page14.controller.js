ca62052ed2932aeee52a4eb2ecb0832f306b8ae7
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-04-16 22:32:26 +0300

    add all

--- app/templates/page14/page14.controller.js
+++ app/templates/page14/page14.controller.js
@@ -5,16 +5,25 @@
         .controller('Page14Controller', Page14Controller);
 
 
-    Page14Controller.$inject = ['$state', 'toastr', '$scope'];
+    Page14Controller.$inject = ['$state', 'toastr', '$scope', '$timeout'];
 
-    function Page14Controller($state, toastr, $scope) {
+    function Page14Controller($state, toastr, $scope, $timeout) {
         let vm = this;
 
         vm.next = next;
 
         function next() {
+            if (vm.page14 == undefined) {
+                toastr.warning('Заповніть усі поля', 'Увага');
+            } else if (vm.page14.test === 4) {
+                toastr.success('Вірно');
+                $timeout(function () {
+                    $state.go('page15')
+                }, 2000);
+            } else {
+                toastr.warning('Вірна відровідь інакша', 'Увага');
+            }
 
-                $state.go('page15')
 
 
         }
